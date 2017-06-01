5a1a5adfad42451bcf5901487f4f339201cc5eb7
Francisco Bento da Silva Junior <bento.francisco@academico.ifrn.edu.br>
2017-06-01 10:35:03 -0300

    Add logo to LoginActivity
    Clean project

--- IFRNMessenger/app/src/main/java/br/com/postero/ifrnmessenger/activities/MainActivity.java
+++ IFRNMessenger/app/src/main/java/br/com/postero/ifrnmessenger/activities/MainActivity.java
@@ -98,15 +98,6 @@ public class MainActivity extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -209,23 +200,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             this.finish();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
